f1b68d7898efdb3fcce8eb9a3ffd5f8e104bec86
vitek-karas <vitek.karas@microsoft.com>
2021-11-10 08:20:52 -0800

    Formatting

--- Mono.Cecil.Cil/Symbols.cs
+++ Mono.Cecil.Cil/Symbols.cs
@@ -1113,9 +1113,7 @@ namespace Mono.Cecil.Cil {
 	public interface ISymbolWriter : IDisposable {
 
 		ISymbolReaderProvider GetReaderProvider ();
-
 		ImageDebugHeader GetDebugHeader ();
-
 		void Write (MethodDebugInformation info);
 	}
 
